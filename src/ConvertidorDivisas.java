31d9de7e6d783d02b999c786d3ab3c1817bd307b
Leonardo <leonardojosezavala.2@outlook.com>
2023-04-07 14:51:56 -0600

    'Agregacion de excepciones y agregar redondearresultado'

--- src/ConvertidorDivisas.java
+++ src/ConvertidorDivisas.java
@@ -3,25 +3,24 @@ import java.text.DecimalFormat;
 
 public class ConvertidorDivisas extends Convertidor {
     private String seleccion;
-    DecimalFormat formato = new DecimalFormat("#.##");
     public ConvertidorDivisas() {
         super();
         String[] tipos = {"Dolar(USD)","Euros(EUR)",
                 "Yen(JPY)","Libras(GBP)","Dolar(AUD)","Franco Suizo(CHF)",
-                "Renminbi(CNH)","Won(KRW)","Quetzal(GTQ)","Colon(CRC)"};
+                "Renminbi(Yuan)","Won(KRW)","Quetzal(GTQ)","Colon(CRC)"};
         setTipos(tipos);
     }
     public void TiposdeDivisas() {
          seleccion = (String) JOptionPane.showInputDialog(null,"¿Qué Moneda desea convertir?","Convertidor"
                 ,JOptionPane.QUESTION_MESSAGE,null, getTipos(),getTipos()[0]);
+
         ConvertirMoneda();
     }
     public void IngresarCantidad(){
         String input;
-        input = JOptionPane.showInputDialog("Ingresa la cantidad de dinero que deseas convertir:");
+        input = JOptionPane.showInputDialog(null, "Ingresa la cantidad de dinero que deseas convertir:");
         if (ValidarNumero(input)){
             setCantidad(Double.parseDouble(input));
-
         }
         else {
             JOptionPane.showMessageDialog(null, "Ingrese un numero valido");
@@ -29,39 +28,52 @@ public class ConvertidorDivisas extends Convertidor {
         }
 
     }
+    protected void redondearResultado(double num) {
+        DecimalFormat decimal = new DecimalFormat("#.###");
+         setResultado(Double.parseDouble(decimal.format(num)));
+        JOptionPane.showMessageDialog(null,"Tienes:" + getResultado() + " " + this.seleccion);
+    }
     public void ConvertirMoneda(){
         switch (this.seleccion){
             case "Dolar(USD)":
                 IngresarCantidad();
-                setResultado(getCantidad() * 0.041);
-                JOptionPane.showMessageDialog(null,"es:" + getResultado());
+                redondearResultado(getCantidad() * 0.041);
                 break;
             case "Euros(EUR)":
-
+                IngresarCantidad();
+                redondearResultado(getCantidad() * 0.037);
                 break;
             case "Yen(JPY)":
-
+                IngresarCantidad();
+                redondearResultado(getCantidad() * 5.38);
                 break;
             case "Libras(GBP)":
-
+                IngresarCantidad();
+                redondearResultado(getCantidad() * 0.033);
                 break;
             case "Dolar(AUD)":
-
+                IngresarCantidad();
+                redondearResultado(getCantidad() * 0.061);
                 break;
             case "Franco Suizo(CHF)":
-
+                IngresarCantidad();
+                redondearResultado(getCantidad() * 0.037);
                 break;
-            case "Renminbi(CNH)":
-
+            case "Renminbi(Yuan)":
+                IngresarCantidad();
+                redondearResultado(getCantidad() * 0.28);
                 break;
             case "Won(KRW)":
-
+                IngresarCantidad();
+                redondearResultado(getCantidad() * 53.55);
                 break;
             case "Quetzal(GTQ)":
-
+                IngresarCantidad();
+                redondearResultado(getCantidad() * 0.32);
                 break;
             case "Colon(CRC)":
-
+                IngresarCantidad();
+                redondearResultado(getCantidad() * 21.89);
                 break;
         }
     }
--- src/MenuPrincipal.java
+++ src/MenuPrincipal.java
@@ -3,18 +3,33 @@ import javax.swing.*;
 public class MenuPrincipal {
     public static void main(String[] args) {
         ConvertidorDivisas divisas = new ConvertidorDivisas();
+        boolean seguir=true;
+        while (seguir==true){
+            String[] tipoConversor ={"Convertidor de Divisas","Convertidor de Temperatura"};
 
-        String[] tipoConversor ={"Convertidor de Divisas","Convertidor de Temperatura"};
+            int Menu =JOptionPane.showOptionDialog(null,"Seleccione un convertir:","Convertidores",
+                    JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null,tipoConversor,tipoConversor[1]);
+            try{
+                switch (Menu){
+                    case 0:
+                        divisas.TiposdeDivisas();
+                        break;
+                    case 1:
 
-        int Menu =JOptionPane.showOptionDialog(null,"Seleccione un convertir:","Convertidores",
-                 JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null,tipoConversor,tipoConversor[1]);
-        switch (Menu){
-            case 0:
-                divisas.TiposdeDivisas();
-                break;
-            case 1:
-
-                break;
+                        break;
+                }
+            }
+            catch (Exception e){
+                System.out.println(e);
+            }
+            int continuar = JOptionPane.showConfirmDialog(null,"¿Desea continuar?");
+            if (continuar==0){
+                seguir=true;
+            }
+            else {
+                JOptionPane.showMessageDialog(null, "Programa Finalizado");
+                seguir=false;
+            }
         }
 
     }
